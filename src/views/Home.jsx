a7a9ddfa0025a3772f1132cbfc06a3d2d35984c2
Matrix53 <1079207272@qq.com>
2021-08-20 23:04:30 +0800

    feat: add banner

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,17 @@
 import { makeStyles } from "@material-ui/core/styles";
+import banner from "assets/banner.png";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "90vh",
+    width: "100vw",
+  },
+}));
 
 function Home() {
   const classes = useStyles();
 
-  return null;
+  return <img src={banner} className={classes.root} />;
 }
 
 export default Home;
