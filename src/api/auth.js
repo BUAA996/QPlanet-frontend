f30e13de823a6ba046c60b8f582cbf346808df16
Matrix53 <1079207272@qq.com>
2021-08-20 22:33:48 +0800

    chore: finish user model

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,2 +1,26 @@
-import axios from 'http.js'
+import axios from "http.js";
 
+// register
+function register(data) {
+  return axios.post("user/logout/", data);
+}
+
+// send
+function getRegisterId(data) {
+  return axios.post("user/send/", data);
+}
+
+// login
+function login(data) {
+  return axios.post("user/login/", data);
+}
+
+// logout
+function logout(data) {
+  return axios.post("user/logout/", data);
+}
+
+// getcaptcha
+function getCaptcha(data) {
+  return axios.post("user/captcha/", data);
+}
