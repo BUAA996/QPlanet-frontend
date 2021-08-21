62aaba0ffdf5f3236a1e31a956fddf39612090a5
Withinlover <2608491128@qq.com>
2021-08-21 15:38:06 +0800

    Add Overview page 1.0

---
+++ src/components/utils/QuestionnaireList.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { Grid } from '@material-ui/core';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: '70vh',
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
+export default function QuestionnaireList() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Grid container className={classes.root}>
+			<Grid item xs={2}>
+				<Tabs
+					orientation="vertical"
+					variant="scrollable"
+					value={value}
+					onChange={handleChange}
+					aria-label="Vertical tabs example"
+					className={classes.tabs}
+				>
+					<Tab label="全部问卷" {...a11yProps(0)} />
+					<Tab label="已发布问卷" {...a11yProps(1)} />
+					<Tab label="已完成问卷" {...a11yProps(2)} />
+				</Tabs>
+			</Grid>
+			<Grid item xs={10}>
+				<TabPanel value={value} index={0}>
+					全部问卷
+				</TabPanel>
+				<TabPanel value={value} index={1}>
+					已发布问卷
+				</TabPanel>
+				<TabPanel value={value} index={2}>
+					已完成问卷
+				</TabPanel>
+			</Grid>
+      
+      
+    </Grid>
+  );
+}
--- src/views/Overview.jsx
+++ src/views/Overview.jsx
@@ -1,11 +1,83 @@
+import { Container, Typography, Grid, Paper, Divider, Button, Box } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import SearchInputButton from "components/utils/SearchInputButton";
+import QuestionnaireList from "components/utils/QuestionnaireList";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    paddingTop: theme.spacing(3),
+    flexGrow: 1,
+  },
+  sideBar: {
+    paddingTop: theme.spacing(2),
+  },
+  sideBarContainer: {
+    padding: 0,
+  },
+  sideBarContainerButton: {
+    height: 60,
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  headBar: {
+    marginTop: theme.spacing(1),
+    padding: theme.spacing(2),
+    fontSize: 22,
+    alignItems: "center",
+  },
+  headBarTitle: {
+    textAlign: 'center',
+    color: theme.palette.primary.dark,
+    fontWeight: 'bold'
+  },
+  test: {
+    backgroundColor: theme.palette.secondary.dark,
+  }
+}));
+
+function SideBar() {
+  const classes = useStyles();
+
+  return (
+    <Container className={classes.sideBarContainer}>
+      <Button variant="contained" color="primary" fullWidth className={classes.sideBarContainerButton}> +&nbsp;新建问卷 </Button>
+    </Container>
+  );
+}
+
+function HeadBar() {
+  const classes = useStyles();
+  return (
+    <Grid container className={classes.headBar}>
+      <Grid item className={classes.headBarTitle} xs={4}>问卷列表</Grid>
+      <Grid item xs={8}> <SearchInputButton /> </Grid>
+    </Grid>
+  );
+}
 
 function Overview() {
   const classes = useStyles();
 
-  return null;
+  return (
+    <Container fixed className={classes.root}>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={2}>
+          <SideBar />
+        </Grid>
+        <Grid item xs={10}>
+          <HeadBar />
+        </Grid>
+        <Grid item xs={12}>
+          <QuestionnaireList />
+        </Grid>
+      </Grid>
+    </Container>
+  );  
 }
 
 export default Overview;
