54f51dcac97ee0857815eae4cb8e273919175974
Matrix53 <1079207272@qq.com>
2021-08-22 17:03:17 +0800

    chore: add dialog

--- src/components/feedback/Completion.jsx
+++ src/components/feedback/Completion.jsx
@@ -1,7 +1,16 @@
 import { makeStyles } from '@material-ui/core/styles'
 import PropTypes from 'prop-types'
 import Template from './Template'
-import { Button } from '@material-ui/core'
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+} from '@material-ui/core'
+import { Close } from '@material-ui/icons'
+import { useState } from 'react'
 
 const useStyles = makeStyles((theme) => ({
   btn: {
@@ -9,31 +18,83 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-function DetailDialog() {
-  return null
+function DetailDialog({ open, setOpen, title }) {
+  return (
+    <Dialog open={open}>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogActions>
+        <IconButton
+          onClick={() => {
+            setOpen(false)
+          }}
+        >
+          <Close />
+        </IconButton>
+      </DialogActions>
+      <DialogContent></DialogContent>
+    </Dialog>
+  )
 }
 
-function FrequencyDialog() {
-  return null
+function FrequencyDialog({ open, setOpen, data }) {
+  return (
+    <Dialog open={open}>
+      <DialogTitle>词频分析</DialogTitle>
+      <DialogActions>
+        <IconButton
+          onClick={() => {
+            setOpen(false)
+          }}
+        >
+          <Close />
+        </IconButton>
+      </DialogActions>
+      <DialogContent></DialogContent>
+    </Dialog>
+  )
 }
 
 function Completion({ data }) {
   const classes = useStyles()
+  const [detailOpen, setDetailOpen] = useState(false)
+  const [frequencyOpen, setFrequencyOpen] = useState(false)
 
   return (
-    <Template title={data.title} type={data.type}>
-      <Button
-        variant='outlined'
-        className={classes.btn}
-        color='primary'
-        size='small'
-      >
-        查看详细信息
-      </Button>
-      <Button size='small' variant='outlined' color='primary'>
-        词频分析
-      </Button>
-    </Template>
+    <>
+      <Template title={data.title} type={data.type}>
+        <Button
+          variant='outlined'
+          className={classes.btn}
+          color='primary'
+          size='small'
+          onClick={() => {
+            setDetailOpen(true)
+          }}
+        >
+          查看详细信息
+        </Button>
+        <Button
+          size='small'
+          variant='outlined'
+          color='primary'
+          onClick={() => {
+            setFrequencyOpen(true)
+          }}
+        >
+          词频分析
+        </Button>
+      </Template>
+      <DetailDialog
+        open={detailOpen}
+        setOpen={setDetailOpen}
+        title={data.title}
+      />
+      <FrequencyDialog
+        open={frequencyOpen}
+        setOpen={setFrequencyOpen}
+        data={data}
+      />
+    </>
   )
 }
 
