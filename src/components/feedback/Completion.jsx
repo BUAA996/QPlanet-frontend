be965f3966855c79cc33fa64269e822028cbe3eb
Matrix53 <1079207272@qq.com>
2021-08-22 09:52:30 +0800

    chore: update choice analysis component

--- src/components/feedback/Completion.jsx
+++ src/components/feedback/Completion.jsx
@@ -1,11 +1,26 @@
 import { makeStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
+import Template from "./Template";
 
 const useStyles = makeStyles((theme) => ({}));
 
-function Completion() {
+function Completion({ data }) {
   const classes = useStyles();
 
-  return null;
+  return (
+    <Template title={data.title} type={data.type}>
+      hahahah
+    </Template>
+  );
 }
+
+Completion.propTypes = {
+  props: PropTypes.shape({
+    choice: PropTypes.arrayOf(PropTypes.string),
+    count: PropTypes.arrayOf(PropTypes.number),
+    title: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+  }),
+};
 
 export default Completion;
