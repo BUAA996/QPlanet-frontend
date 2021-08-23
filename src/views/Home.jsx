1ddb0ffaa15f2ceca922d710bee855a45ad0025f
Matrix53 <1079207272@qq.com>
2021-08-24 01:57:22 +0800

    feat: update home page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,17 @@
 import { makeStyles } from '@material-ui/core/styles'
 import banner from 'assets/banner.png'
 import useTitle from 'hooks/useTitle'
+import { Typography, Grid } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: '89vh',
-    width: '99vw',
+    backgroundImage: `url(${banner})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: '99vw 89vh',
   },
+  title: { marginTop: '10vh' },
+  subTitle: { marginTop: '4vh' },
 }))
 
 function Home() {
@@ -14,7 +19,18 @@ function Home() {
 
   useTitle('首页 - 问卷星球')
 
-  return <img src={banner} className={classes.root} alt='banner' />
+  return (
+    <Grid xs={12} container className={classes.root} justifyContent='center'>
+      <Grid item xs={6} container direction='column' alignItems='center'>
+        <Typography className={classes.title} color='primary' variant='h3'>
+          问卷调查，从"星"开始
+        </Typography>
+        <Typography className={classes.subTitle} color='primary'>
+          一分钟快速注册，即刻体验便捷的问卷调查与数据分析平台
+        </Typography>
+      </Grid>
+    </Grid>
+  )
 }
 
 export default Home
