f09cefe73b618dbd04c8e5d3247e6c8a07935025
Matrix53 <1079207272@qq.com>
2021-08-24 09:01:13 +0800

    chore: prepare to add change password

--- src/components/utils/ChangePassword.jsx
+++ src/components/utils/ChangePassword.jsx
@@ -1,11 +1,45 @@
 import { makeStyles } from '@material-ui/core/styles'
+import {
+  Dialog,
+  DialogTitle,
+  Box,
+  DialogActions,
+  IconButton,
+  DialogContent,
+  TextField,
+} from '@material-ui/core'
+import { Close } from '@material-ui/icons'
 
 const useStyles = makeStyles((theme) => ({}))
 
-function ChangePassword() {
+function ChangePassword({ open, setOpen }) {
   const classes = useStyles()
 
-  return null
+  return (
+    <Dialog open={open} maxWidth='sm'>
+      <Box
+        display='flex'
+        justifyContent='center'
+        width={600}
+        position='relative'
+      >
+        <DialogTitle>修改密码</DialogTitle>
+        <DialogActions className={classes.closeBtn}>
+          <IconButton
+            onClick={() => {
+              setOpen(false)
+            }}
+          >
+            <Close />
+          </IconButton>
+        </DialogActions>
+      </Box>
+      <DialogContent>
+        <TextField></TextField>
+        <TextField></TextField>
+      </DialogContent>
+    </Dialog>
+  )
 }
 
 export default ChangePassword
