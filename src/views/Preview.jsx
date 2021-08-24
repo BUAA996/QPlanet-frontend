14fc9b74b8b9ec0a4a0d5c7174cd951ab6283333
Matrix53 <1079207272@qq.com>
2021-08-24 11:28:55 +0800

    feat: add q donwload

--- src/views/Preview.jsx
+++ src/views/Preview.jsx
@@ -14,6 +14,9 @@ import Problem from 'components/utils/Problem'
 import useTitle from 'hooks/useTitle'
 import { useParams } from 'react-router'
 import { useHistory } from 'react-router-dom'
+import SpeedDialMenu from 'components/utils/SpeedDialMenu'
+import { downloadQuestionnaire } from 'api/questionaire'
+import { download } from 'utils'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -49,35 +52,40 @@ const useStyles = makeStyles((theme) => ({
 
 const QUESTIONAIRE = [
   {
-    id: 1, key: 1,
+    id: 1,
+    key: 1,
     kind: 0,
     must: 1,
     title: '第一题 balabalabalabala',
     choices: ['选项1', '选项2', '选项3', '选项4'],
   },
   {
-    id: 2, key: 2,
+    id: 2,
+    key: 2,
     kind: 1,
     must: 1,
     title: '第二题 balabalabalabala',
     choices: ['选项1', '选项2', '选项3', '选项4'],
   },
   {
-    id: 3, key: 3,
+    id: 3,
+    key: 3,
     kind: 2,
     must: 0,
     title: '第三题',
     choices: [],
   },
   {
-    id: 4, key: 4,
+    id: 4,
+    key: 4,
     kind: 1,
     must: 0,
     title: '第四题',
     choices: ['选项1', '选项2', '选项3', '选项4'],
   },
   {
-    id: 5, key: 5,
+    id: 5,
+    key: 5,
     kind: 2,
     must: 1,
     title: '第五题',
@@ -99,18 +107,19 @@ const ANSJSON = {
       answer: ['test2-1', 'test2-2'],
     },
     {
-      "problem_id": 1,
-      "type": 2,
-      "answer": ["test3",],
+      problem_id: 1,
+      type: 2,
+      answer: ['test3'],
     },
-  ]
+  ],
 }
 
 const TITLE = '给zht买女装 & lls小课堂 的问卷调查'
-const DESCRIPTION = '感谢您能抽时间参与本次问卷，您的意见和建议就是我们前行的动力！'
+const DESCRIPTION =
+  '感谢您能抽时间参与本次问卷，您的意见和建议就是我们前行的动力！'
 
 function Title(props) {
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
     <>
@@ -121,11 +130,11 @@ function Title(props) {
         <Typography varient='h6'>{props.description}</Typography>
       </Grid>
     </>
-  );
+  )
 }
 
 function PreviewPage(props) {
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
     <Card className={classes.card}>
@@ -147,35 +156,35 @@ function PreviewPage(props) {
         </Grid>
       </Grid>
     </Card>
-  );
+  )
 }
 
-function Preview() {  
+function Preview() {
   useTitle('预览问卷 - 问卷星球')
 
-  const classes = useStyles();
-  const [questionID, setID] = useState(-1);
-  const [title, setTitle] = useState('');
-  const [Questionare, setQuestionare] = useState([]);
-  const [description, setDescription] = useState('');
-  const [ansList, setAns] = useState([]);
-  const { id } = useParams();
-  const history = useHistory();
+  const classes = useStyles()
+  const [questionID, setID] = useState(-1)
+  const [title, setTitle] = useState('')
+  const [Questionare, setQuestionare] = useState([])
+  const [description, setDescription] = useState('')
+  const [ansList, setAns] = useState([])
+  const { id } = useParams()
+  const history = useHistory()
 
   useEffect(() => {
-    setTitle(TITLE);
-    setDescription(DESCRIPTION);
+    setTitle(TITLE)
+    setDescription(DESCRIPTION)
     setQuestionare([].concat(QUESTIONAIRE))
 
-    view({"hash": id}).then((res) => {
+    view({ hash: id }).then((res) => {
       // console.log(res);
       if (res.data.result === 1) {
-        const ori = res.data.questions;
-        const settings = res.data;
-        let tmp = [];
-        for (let i = 0;i < ori.length; ++i) {
+        const ori = res.data.questions
+        const settings = res.data
+        let tmp = []
+        for (let i = 0; i < ori.length; ++i) {
           tmp.push({
-            id: ori[i].id, 
+            id: ori[i].id,
             key: i,
             description: ori[i].description,
             kind: ori[i].type,
@@ -185,35 +194,33 @@ function Preview() {
           })
         }
         setQuestionare([].concat(tmp))
-        setTitle(settings.title);
-        setDescription(settings.description);
-        setID(settings.qid);
-        
-        tmp = new Array(ori.length);
-        for (let i = 0;i < ori.length; ++i) {
+        setTitle(settings.title)
+        setDescription(settings.description)
+        setID(settings.qid)
+
+        tmp = new Array(ori.length)
+        for (let i = 0; i < ori.length; ++i) {
           tmp[i] = {
             problem_id: ori[i].id,
             type: ori[i].type,
             answer: [''],
           }
         }
-        setAns(tmp);
+        setAns(tmp)
       } else {
         history.push('/404')
       }
     })
   }, [])
 
-
-
   function handleAns(id, singleAns) {
-    let tmp = [].concat(ansList); 
+    let tmp = [].concat(ansList)
     tmp[id] = {
       problem_id: tmp[id].problem_id,
       type: tmp[id].type,
       answer: singleAns,
     }
-    setAns(tmp);
+    setAns(tmp)
     // console.log(tmp);
   }
 
@@ -226,14 +233,15 @@ function Preview() {
   }
 
   const print = () => {
-    window.document.body.innerHTML = window.document.getElementById('billDetails').innerHTML;  
-    window.print(); 
-    window.location.reload();
-}
+    window.document.body.innerHTML =
+      window.document.getElementById('billDetails').innerHTML
+    window.print()
+    window.location.reload()
+  }
 
   return (
     <>
-      <Container maxWidth='md' className={classes.root} id="billDetails">
+      <Container maxWidth='md' className={classes.root} id='billDetails'>
         <Card className={classes.card}>
           <Grid
             container
@@ -242,29 +250,61 @@ function Preview() {
             alignItems='center'
             spacing={3}
           >
-            <Grid item>
-              提示：在预览状态下不可提交问卷
-            </Grid>
-            <Title title={title} description={description}/>
+            <Grid item>提示：在预览状态下不可提交问卷</Grid>
+            <Title title={title} description={description} />
             <Divider
               flexItem={true}
               variant={'middle'}
               className={classes.divider}
             />
             <Grid item className={classes.problems}>
-              {Questionare.map((problem) => <Problem problem={problem} key={problem.key} updateAns={(ans) => handleAns(problem.key, ans)} />)}
+              {Questionare.map((problem) => (
+                <Problem
+                  problem={problem}
+                  key={problem.key}
+                  updateAns={(ans) => handleAns(problem.key, ans)}
+                />
+              ))}
             </Grid>
             <Grid item className={classes.buttons}>
-              <Button variant='contained' color='secondary' onClick={() => handleClick()} className={classes.buttons}> 提交 </Button>
-              <Button variant='contained' color='secondary' onClick={() => history.go(-1)} className={classes.buttons}> 返回 </Button>
+              <Button
+                variant='contained'
+                color='secondary'
+                onClick={() => handleClick()}
+                className={classes.buttons}
+              >
+                {' '}
+                提交{' '}
+              </Button>
+              <Button
+                variant='contained'
+                color='secondary'
+                onClick={() => history.go(-1)}
+                className={classes.buttons}
+              >
+                {' '}
+                返回{' '}
+              </Button>
               {/* <Button variant='contained' color='secondary' onClick={() => print()} className={classes.buttons}> 打印 </Button> */}
             </Grid>
           </Grid>
         </Card>
       </Container>
+      <SpeedDialMenu
+        handleClick={(name) => {
+          if (name === '下载问卷') {
+            downloadQuestionnaire({ hash: id }).then((res) => {
+              download(
+                'https://api.matrix53.top/img/' + res.data.doc_name,
+                res.data.doc_name
+              )
+            })
+          }
+        }}
+      />
     </>
   )
 }
 
 export default Preview
-export {Title, PreviewPage}
+export { Title, PreviewPage }
