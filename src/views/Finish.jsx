f454c86aac8a87cf0ee0c7dc7efde6149aa44599
Matrix53 <1079207272@qq.com>
2021-08-26 11:03:24 +0800

    refactor: finish page update

--- src/views/Finish.jsx
+++ src/views/Finish.jsx
@@ -1,9 +1,6 @@
-import Problem from 'components/utils/Problem'
-import { Button, CardContent, Container } from '@material-ui/core'
+import { Card, Grid, Container, Typography } from '@material-ui/core'
 import useTitle from 'hooks/useTitle'
-import {Title, PreviewPage} from 'views/Preview'
 import { makeStyles } from '@material-ui/core/styles'
-import { Link as RouterLink } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -39,56 +36,29 @@ const useStyles = makeStyles((theme) => ({
 
 function Finish() {
   useTitle('填写已完成')
-  const Questionare = [
-    {
-      id: 1,
-      kind: 0,
-      must: 1,
-      title: '第一题 balabalabalabala',
-      choices: ['选项1', '选项2', '选项3', '选项4'],
-    },
-    {
-      id: 2,
-      kind: 1,
-      must: 1,
-      title: '第二题 balabalabalabala',
-      choices: ['选项1', '选项2', '选项3', '选项4'],
-    },
-    {
-      id: 3,
-      kind: 2,
-      must: 0,
-      title: '第三题',
-      choices: [],
-    },
-    {
-      id: 4,
-      kind: 1,
-      must: 0,
-      title: '第四题',
-      choices: ['选项1', '选项2', '选项3', '选项4'],
-    },
-    {
-      id: 5,
-      kind: 2,
-      must: 1,
-      title: '第五题',
-      choices: ['选项1', '选项2', '选项3', '选项4'],
-    },
-  ]
 
-  
-  function blankFunction() {}
-
-  const classes = useStyles();
-  const title = <Title title="填写完成" description=""/>  
-  const Questions = Questionare.map((problem) => (<Problem problem={problem} updateAns={(ans) => blankFunction()} />));
+  const classes = useStyles()
 
   return (
     <>
-
-      <Container maxWidth="md" className={classes.root}>
-        <PreviewPage title={title} Questionare={null}/>
+      <Container maxWidth='md' className={classes.root}>
+        <Card className={classes.card}>
+          <Grid
+            container
+            direction='column'
+            justifyContent='center'
+            S
+            alignItems='center'
+            spacing={3}
+          >
+            <Grid item className={classes.title}>
+              <Typography variant='h4'>填写完成</Typography>
+            </Grid>
+            <Grid item className={classes.description}>
+              <Typography varient='h6'></Typography>
+            </Grid>
+          </Grid>
+        </Card>
       </Container>
     </>
   )
