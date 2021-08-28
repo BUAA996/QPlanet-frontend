834c8c25e30497e686a46e0ad7c9c653faba9866
luxia <allxst@me.com>
2021-08-28 22:26:17 +0800

    feat : can edit deadline

--- src/components/design/FormDialog.jsx
+++ src/components/design/FormDialog.jsx
@@ -1,11 +1,5 @@
 import React, {useState} from 'react';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import {CreateRounded} from '@material-ui/icons';
 import {Grid} from '@material-ui/core';
 import {useEffect} from 'react';
@@ -43,9 +37,6 @@ export default function FormDialog(props) {
     setDescription(props.description);
   };
 
-  const checkError = (msg) => {
-
-  }
 
   const handleModify = () => {
     if (title.trim() === "") {
@@ -67,6 +58,7 @@ export default function FormDialog(props) {
 
     props.setTitle(title.trim());
     props.setDescription(description.trim());
+    props.setSettings(settings)
 
     setOpen(false);
   }
