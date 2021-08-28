0ed8325d587e5da96a0a25e32260eddc3423ada8
Matrix53 <1079207272@qq.com>
2021-08-28 23:43:25 +0800

    chore: update cross analyse

--- src/components/feedback/Cross.jsx
+++ src/components/feedback/Cross.jsx
@@ -1,13 +1,21 @@
 import { makeStyles } from '@material-ui/core/styles'
-import { Card, TextField, MenuItem, Box } from '@material-ui/core'
+import { Card, TextField, MenuItem, Box, Button } from '@material-ui/core'
 import { useState } from 'react'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     marginTop: theme.spacing(7.7),
-    padding: theme.spacing(2),
+    padding: theme.spacing(4),
     display: 'flex',
-    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    '& .MuiButton-containedPrimary': {
+      color: 'white',
+      fontSize: '1.03em',
+    },
+  },
+  input: {
+    width: '48%',
   },
 }))
 
@@ -15,10 +23,11 @@ function Cross({ qid, choice }) {
   const classes = useStyles()
   const [first, setFirst] = useState('')
   const [second, setSecond] = useState('')
+  const [showGraph, setShowGraph] = useState(false)
 
   return (
     <Card className={classes.root}>
-      <Box>
+      <Box width='100%' display='flex' justifyContent='space-between'>
         <TextField
           select
           label='自变量'
@@ -26,6 +35,7 @@ function Cross({ qid, choice }) {
           onChange={(event) => {
             setFirst(event.target.value)
           }}
+          className={classes.input}
         >
           {choice.map((item) => (
             <MenuItem key={item.id} value={item.id}>
@@ -40,6 +50,7 @@ function Cross({ qid, choice }) {
           onChange={(event) => {
             setSecond(event.target.value)
           }}
+          className={classes.input}
         >
           {choice.map((item) => (
             <MenuItem key={item.id} value={item.id}>
@@ -47,6 +58,41 @@ function Cross({ qid, choice }) {
             </MenuItem>
           ))}
         </TextField>
+      </Box>
+      <Box
+        display='flex'
+        width='100%'
+        marginTop='3.5%'
+        justifyContent='space-between'
+      >
+        <Button
+          color='primary'
+          variant='contained'
+          onClick={() => {
+            setShowGraph((showGraph) => !showGraph)
+          }}
+        >
+          交叉分析
+        </Button>
+        {showGraph && (
+          <>
+            <Button color='primary' variant='contained'>
+              表格
+            </Button>
+            <Button color='primary' variant='contained'>
+              柱状图
+            </Button>
+            <Button color='primary' variant='contained'>
+              条形图
+            </Button>
+            <Button color='primary' variant='contained'>
+              雷达图
+            </Button>
+            <Button color='primary' variant='contained'>
+              折线图
+            </Button>
+          </>
+        )}
       </Box>
     </Card>
   )
