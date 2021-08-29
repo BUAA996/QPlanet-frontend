c6c7035a57d35720d544860df783c715e896b7f6
Matrix53 <1079207272@qq.com>
2021-08-29 19:36:48 +0800

    feat: add keydown listener to search button

--- src/components/utils/SearchInputButton.jsx
+++ src/components/utils/SearchInputButton.jsx
@@ -2,11 +2,8 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import InputBase from '@material-ui/core/InputBase'
-import Divider from '@material-ui/core/Divider'
 import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
 import SearchIcon from '@material-ui/icons/Search'
-import DirectionsIcon from '@material-ui/icons/Directions'
 import { useState } from 'react'
 
 const useStyles = makeStyles((theme) => ({
@@ -35,10 +32,8 @@ export default function SearchInputButton(props) {
   const [value, setValue] = useState('')
   const handleChange = (event) => {
     setValue(event.target.value)
-    console.log(event.target.value)
   }
   const search = (event) => {
-    console.log('search')
     props.search(value)
   }
 
@@ -49,6 +44,11 @@ export default function SearchInputButton(props) {
         placeholder='搜索问卷'
         value={value}
         onChange={handleChange}
+        onKeyDown={(event) => {
+          if (event.code === 'Enter') {
+            props.search(value)
+          }
+        }}
         inputProps={{ 'aria-label': '搜索问卷' }}
       />
       <IconButton
